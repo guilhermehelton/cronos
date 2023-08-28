8d4c3b64a6a8a279941973cbbae4bb48faeea756
root <root@Guilherme>
2023-08-28 18:05:53 -0300

    commit inicial backend

---
+++ backend/src/main/java/com/cronos/api/CronosApiApplication.java
@@ -0,0 +1,13 @@
+package com.cronos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronosApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cronos/api/CronosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cronos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
